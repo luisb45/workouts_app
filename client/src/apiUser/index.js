94c402caec9f953870492f95fb92577e47d1714c
Luis Bustos <luisbustos500@gmail.com>
2023-02-01 15:47:21 -0700

    deploying app

--- client/src/apiUser/index.js
+++ client/src/apiUser/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/users';
+const url = 'https://workout-app-server.onrender.com/users';
 
 export const fetchUsers = () => axios.get(url);
 export const createUser = (newUser) => axios.post(url, newUser);
